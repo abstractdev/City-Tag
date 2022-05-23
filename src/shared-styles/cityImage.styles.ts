1decf93d518d350e2e172b8ba1e3c4cf45f73df6
abstractdev <fadcny@gmail.com>
2022-05-23 18:39:38 -0400

    initial

---
+++ src/shared-styles/cityImage.styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { vFlex } from "./vFlex.styles";
+
+export const StyledCityImage = styled.img`
+  max-width: 100%;
+  height: auto;
+  object-fit: contain;
+`;
+export const StyledCityImageContainer = styled(vFlex)`
+  width: 100%;
+  height: auto;
+`;
