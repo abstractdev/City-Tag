75faf8bc1f01775a9a7eede52184ccaf32b93f16
abstractdev <fadcny@gmail.com>
2022-05-25 13:23:54 -0400

    add interface

--- src/interfaces/stylesInterface.ts
+++ src/interfaces/stylesInterface.ts
@@ -1,4 +1,36 @@
 export interface stylesInterface {
   filteredCityColor?: string;
   filteredCityFont?: string;
+  boardIsClicked?: boolean;
+  targetPosition?: number[];
+  dropdownPosition?: number[];
+  currentGame?: {
+    name: string;
+    displayName: string;
+    color: string;
+    gameImage: any;
+    gameIsActive: boolean;
+    items: [
+      {
+        name: string;
+        text: string;
+        image: any;
+      },
+      {
+        name: string;
+        text: string;
+        image: any;
+      },
+      {
+        name: string;
+        text: string;
+        image: any;
+      },
+      {
+        name: string;
+        text: string;
+        image: any;
+      }
+    ];
+  } | null;
 }
