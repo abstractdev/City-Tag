1618687687a3fbe0071b89b468679d9be7a7f74a
abstractdev <fadcny@gmail.com>
2022-05-24 21:22:51 -0400

    change filename

---
+++ src/shared-styles/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    main: "#121212",
+    white: "#fff",
+    ny: "#f2c205",
+    paris: "#f94910",
+    rio: "#00ad73",
+    tokyo: "#d78ebf",
+  },
+};
+
+export { theme };
