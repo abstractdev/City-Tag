c71615f3d3c3f91999b1dcfac882f95b73288237
abstractdev <fadcny@gmail.com>
2022-05-26 13:24:58 -0400

    fix typescript operator

--- src/helpers/getGameData.ts
+++ src/helpers/getGameData.ts
@@ -4,7 +4,7 @@ export function getGameItemTextArr(currentGame: any) {
   const game = allGames.find((e) => {
     return e.name === currentGame.name;
   });
-  return game?.items.map((e) => {
+  return game!.items.map((e) => {
     return {
       name: e.name,
       text: e.text,
@@ -15,9 +15,9 @@ export function getGameItemTextArr(currentGame: any) {
 //compare filtered firebase users with existing city object to find name and color
 export function getFilteredCityProperties(userCity: string) {
   const filteredCityObject = allGames.find((e: any) => e.name === userCity);
-  const filteredCityName = filteredCityObject?.displayName;
-  const filteredCityColor = filteredCityObject?.color;
-  const filteredCityFont = filteredCityObject?.name;
+  const filteredCityName = filteredCityObject!.displayName;
+  const filteredCityColor = filteredCityObject!.color;
+  const filteredCityFont = filteredCityObject!.name;
   return {
     filteredCityName,
     filteredCityColor,
