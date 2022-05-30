432bf0c96115e9f3aca752f5311db13c0b508202
abstractdev <fadcny@gmail.com>
2022-05-30 19:44:14 -0400

    modify sort and display

--- src/helpers/makeUserList.tsx
+++ src/helpers/makeUserList.tsx
@@ -1,16 +1,19 @@
 import { StyledTd, StyledTimeTd } from "../shared-styles/leaderboardTable";
+import { convertMsToDisplayTime } from "./convertMsToDisplayTime";
 import { trimData } from "./trimData";
 
 export function makeUserList(userData: any) {
   const sortedUserData = userData!.sort((a: any, b: any) => {
-    return a.time! - b.time!;
+    return a.backendTime! - b.backendTime!;
   });
   return sortedUserData.map((e: any, i: number) => {
     return (
       <tr key={e.id}>
         <StyledTd>{i + 1}</StyledTd>
         <StyledTd>{e.name}</StyledTd>
-        <StyledTimeTd>{trimData(e.displayTime)}</StyledTimeTd>
+        <StyledTimeTd>
+          {trimData(convertMsToDisplayTime(e.backendTime))}
+        </StyledTimeTd>
       </tr>
     );
   });
