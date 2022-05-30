f66778225ae91dd4f6d3303959e2cf895bec0709
abstractdev <fadcny@gmail.com>
2022-05-30 19:36:27 -0400

    get user data on page load

---
+++ src/custom-hooks/useGetUserData.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { getDocs, deleteDoc, doc, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
+
+export function useGetUserData() {
+  const [userData, setUserData] = useState<any>([]);
+
+  useEffect(() => {
+    let temp: any = [];
+    const getUserDataFromFirebase = (async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((e) => {
+          if (!e.data().backendTime) {
+            deleteDoc(doc(db, "users", e.data().id));
+          } else {
+            temp.push(e.data());
+          }
+        });
+        setUserData([...temp]);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+  return { userData };
+}
