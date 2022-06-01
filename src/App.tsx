cb08c3fa1a883695c0505e513246b4ca826b6142
abstractdev <fadcny@gmail.com>
2022-06-01 18:34:53 -0400

    fix end game bug

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
             />
           }
         />
-        {currentGame && gameIsActive && (
+        {currentGame && (
           <Route
             path="/game/:cityParam"
             element={
